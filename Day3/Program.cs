5ae959c423d68f93f08dcb3bc359209ebd8c42c8
totoka28 <totoka282@gmail.com>
2024-12-03 22:55:39 +0100

    day 3 kész

---
+++ Day3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long sum = 0;
+            string[] temp = File.ReadAllLines("adat.txt");
+            //string pattern = @"mul\(\d{1,3},\d{1,3}\)";
+            string pattern = @"(mul\(\d{1,3},\d{1,3}\))|(don't\(\)|do\(\))";
+            Regex regex = new Regex(pattern);
+            string all = "";
+            foreach (var item in temp)
+            {
+                all = all + item;
+            }
+            
+            MatchCollection minden = regex.Matches(all);
+            bool dont = false;
+            foreach (var alma in minden)
+            {
+                Console.WriteLine(alma);
+                if (alma.ToString() == "don't()")
+                {
+                    dont = true;
+                }
+                else if (alma.ToString() == "do()")
+                {
+                    dont = false;
+                }
+                else
+                {
+                    if (!dont)
+                    {
+                        string splitelendo = alma.ToString().Substring(4, alma.ToString().Length - 5);
+                        sum = sum + (Convert.ToInt32(splitelendo.Split(',')[0]) * Convert.ToInt32(splitelendo.Split(',')[1]));
+                    }
+                }     
+            }
+            
+            Console.WriteLine(sum);
+            Console.Read();
+        }
+    }
+}
--- Day3/Properties/AssemblyInfo.cs
+++ Day3/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("test")]
+[assembly: AssemblyTitle("Day3")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("test")]
+[assembly: AssemblyProduct("Day3")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0203dc8d-6de7-4f1d-aaf5-4d5ac2a04173")]
+[assembly: Guid("ff62c2a1-f0c1-4491-8c30-e48c99d39944")]
 
 // Version information for an assembly consists of the following four values:
 //
